f6fa62d527d7bf2e8bb24403530d8a2256152f27
aboneda <abdelrahmanaboneda@gmail.com>
2023-07-22 18:19:32 +0200

    update recommend

--- src/pages/recommend.tsx
+++ src/pages/recommend.tsx
@@ -19,10 +19,6 @@ export default function Recommend() {
     const {date,setdate,recommendedmovies, setrecommendedmovies} = useSearchProps()
     const router = useRouter()
 
-
-    if(!currentUser){
-      router.push("/auth/login")
-    }
     const getRecommendedMovies = async ()=>{
 
       const searchedMovies = await getUserMovies(currentUser?.uid);
